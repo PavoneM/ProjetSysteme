95fab6197c27c16941ff45c7befbeb870bb8cd7e
Cindy Najjar <najjar@polytech.unice.fr>
2015-06-08 17:25:54 +0200

    création+destruction cache list

--- cache_list.c
+++ cache_list.c
@@ -7,49 +7,57 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "cache_list.h"
 #include "stdbool.h"
 
-static Cache_List *cache_list = NULL;
-
 /** Crée et initialise une nouvelle liste (vide) et retourne un pointeur dessus */
 struct Cache_List *Cache_List_Create() {
-
+	
+	struct Cache_List *cache_list = malloc(sizeof(struct Cache_List));
+	//cache_list est vide
+	cache_list->pheader = NULL;
+	cache_list->prev = NULL; 
+	cache_list->next = NULL;
+	return cache_list;
 }
 
 /** Détruit la liste pointée par list */
-void Cache_List_Delete(struct Cache_List *list) {}
+void Cache_List_Delete(struct Cache_List *list) {
+	struct Cache_List *cur, *suiv;
 
-/**Crée une nouvelle cellule de liste contenant le pointeur pbh et l’insère en queue de liste */
+	//d'abord on vide la liste
+    for (cur = list->next; cur != list; cur = suiv) {
+        suiv = cur->next; //on enregistre le pointeur dans suiv
+        free(cur); //on supprime l'élément courant
+    }
+    
+    //ensuite on détruit la liste
+    free(list);
+}
+
+/*! Insertion d'un élément à la fin */
 void Cache_List_Append(struct Cache_List *list, struct Cache_Block_Header *pbh) {}
-
-/**Crée une nouvelle cellule de liste contenant le pointeur pbh et l’insère en tête de liste */
+/*! Insertion d'un élément au début*/
 void Cache_List_Prepend(struct Cache_List *list, struct Cache_Block_Header *pbh) {}
 
-/** Retourne le pointeur sur le premier entête de la liste et détruit la
- * cellule correspondante de la liste. Retourne NULL si la liste est vide. */
-struct Cache_Block_Header*Cache_List_Remove_First(struct Cache_List *list)  {}
+/*! Retrait du premier élément */
+struct Cache_Block_Header *Cache_List_Remove_First(struct Cache_List *list) {}
+/*! Retrait du dernier élément */
+struct Cache_Block_Header *Cache_List_Remove_Last(struct Cache_List *list) {}
+/*! Retrait d'un élément quelconque */
+struct Cache_Block_Header *Cache_List_Remove(struct Cache_List *list,
+                                             struct Cache_Block_Header *pbh) {}
 
-/** Retourne le pointeur sur le dernier entête de la liste et détruit la
- * cellule correspondante de la liste. Retourne NULL si la liste est vide. */
-struct Cache_Block_Header *Cache_List_Remove_Last(struct Cache_List *list, struct Cache_Block_Header *pbh) {}
-
-/** Cherche la cellule contenant le pointeur pbh et la retire de la liste. 
- * Retourne pbh (ou NULL si la liste est vide ou si elle ne contient pas pbh). */
-struct Cache_Block_Header *Cache_List_Remove(struct Cache_List *list, struct Cache_Block_Header *pbh) {}
-
-/** Détruit toutes les cellules de la liste, qui redevient donc vide. */
+/*! Remise en l'état de liste vide */
 void Cache_List_Clear(struct Cache_List *list) {}
 
-/** Retourne true si la liste est vide, false sinon. */
-boolean Cache_List_Is_Empty(struct Cache_List *list) {}
+/*! Test de liste vide */
+bool Cache_List_Is_Empty(struct Cache_List *list) {}
 
-/** Cherche la cellule contenant le pointeur pbh et la transfère en queue de liste. 
- * Si la liste ne contient pas pbh, ce dernier est ajouté à la fin de la liste. Si
-pbh est déjà à sa position de destination, rien ne se produit. */
-void Cache_List_Move_To_End(struct Cache_List *list, struct Cache_Block_Header *pbh) {}
-
-/** Cherche la cellule contenant le pointeur pbh et la transfère en tête de liste. 
- * Si la liste ne contient pas pbh, ce dernier est ajouté en tête de la liste. Si
-pbh est déjà à sa position de destination, rien ne se produit. */
-void Cache_List_Move_To_Begin(struct Cache_List *list, struct Cache_Block_Header *pbh) {}
+/*! Transférer un élément à la fin */
+void Cache_List_Move_To_End(struct Cache_List *list,
+                            struct Cache_Block_Header *pbh) {}
+/*! Transférer un élément  au début */
+void Cache_List_Move_To_Begin(struct Cache_List *list,
+                              struct Cache_Block_Header *pbh) {}
