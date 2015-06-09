f1c2e61a31b2db65f53e4b871067d73a6c9daa86
Cindy Najjar <najjar@polytech.unice.fr>
2015-06-09 09:28:58 +0200

    correction erreurs

--- cache_list.c
+++ cache_list.c
@@ -14,12 +14,12 @@
 struct Cache_List *Cache_List_Create() {
 	
 	struct Cache_List *cache_list = (struct Cache_List*) malloc(sizeof(struct Cache_List));
-	struct Cache_Block_Header header;
+	struct Cache_Block_Header *header = NULL;
 	
 	//cache_list est vide			
 	cache_list->pheader = header;
-	cache_list->prev = cache_list; 
-	cache_list->next = cache_list;
+	cache_list->prev = NULL; 
+	cache_list->next = NULL;
 	return cache_list;
 }
 
@@ -40,18 +40,29 @@ void Cache_List_Delete(struct Cache_List *list) {
 /*! Insertion d'un élément à la fin */
 void Cache_List_Append(struct Cache_List *list, struct Cache_Block_Header *pbh) {
 	struct Cache_List *new = (struct Cache_List*) malloc(sizeof(struct Cache_List));
+	struct Cache_List *first_element = list; //premier élément de list
+	struct Cache_List *last_element = list->prev; //(list->prev) = dernier élément car la liste est doublement chaînée
 	
-	//on refait les chainages
-	(list->prev)->next = new;
-	new->prev = list->prev;
-	list->prev = new;
-	new->next = list;
+	//on refait les chaînages
+	last_element->next=new;
+	new->prev = last_element; //l'élément précédant le nouvel élément est l'ancien dernier de list 
+	new->next = first_element; //l'élément suivant new est le premier élément car la liste est doublement chaînée
 	new->pheader = pbh;
+	last_element = new; 
 }
 
 /*! Insertion d'un élément au début*/
 void Cache_List_Prepend(struct Cache_List *list, struct Cache_Block_Header *pbh) {
-	struct Cache_List *new = malloc(sizeof(struct Cache_List));S
+	struct Cache_List *new = malloc(sizeof(struct Cache_List));
+	struct Cache_List *first_element = list; 
+	struct Cache_List *last_element = list->prev;
+	
+	//on refait les chaînages
+	first_element->prev = new; //l'élément précédant le premier élément de list devient le nouveau premier élément
+	new->next = first_element;
+	new->prev = last_element;
+	new->pheader = pbh;
+	first_element = new;
 }
 
 /*! Retrait du premier élément */
