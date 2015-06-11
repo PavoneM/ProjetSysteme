cb16456498ce2b9a2f5f415564a85c64522d18e9
Thomas Moras <moras.thomas@gmail.com>
2015-06-11 22:38:14 +0200

    NUR (a tester pour confirmer)

--- NUR_strategy.c
+++ NUR_strategy.c
@@ -25,7 +25,7 @@ void Strategy_Close(struct Cache *pcache) {}
 void Strategy_Invalidate(struct Cache *pcache) {
     int ib;
     for(ib =0; ib<pcache->nblocks; ib++) {
-        pcache->headers[i].flags = pcache->headers[i].flags & ~REFER;
+        pcache->headers[ib].flags = pcache->headers[ib].flags & ~REFER;
     }
     // Initialisation du compteur puis incrementation du nombre de déréférençages.
     pcache->pstrategy = 0;
@@ -34,45 +34,61 @@ void Strategy_Invalidate(struct Cache *pcache) {
 
 // Determine la valeur qui permet de choisir le bloc à utiliser (moins utilisé)
 
-int calcul(struct Cache_Block_Header *bloc_non_vide) {
+void calcul(struct Cache_Block_Header *bloc) {
 
-    n=2*bloc_non_vide->flags.REFER + bloc_non_vide->flags.MODIF;
-    return n
+    int n = 0;
+    if ((bloc->flags & REFER) == 16) {
+        n = n + 2;
+    }
+    if ((bloc->flags & MODIF) == 4){
+        n = n + 1;
+    }
+
+    return n;
 }
 
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) {
 
     struct Cache_Block_Header *bloc_vide = NULL;
     struct Cache_Block_Header *bloc_non_vide;
-    int min=3;
+    int min=4;
+    int ib;
 
     // On cherche d'abord un bloc invalide
+
     if ((bloc_vide = Get_Free_Block(pcache)) != NULL) return bloc_vide;
 
     // Sinon on détermine le bloc qui à été le moins utilisé
-    for(ib =0; ib<pcache->nblocks; ib++) {
-        int n=calcul(pcache->headers[ib]);
-        if(n==0)
-            return bloc_non_vide
-        else if (n<min) {
-            min=n;
-            bloc_non_vide =	pcache->headers[ib]
-            else
-            n=3;
-            bloc_non_vide =	pcache->headers[ib]
+
+    for(ib =0; ib < pcache->nblocks; ib++) {
+
+        int n=calcul(&pcache->headers[ib]);
+        if(n==0) {
+            bloc_non_vide = &pcache->headers[ib];
+            return bloc_non_vide;
 
         }
-        return bloc_non_vide;
+        else {
+            if(n<min) {
+                min = n;
+                bloc_non_vide = &pcache->headers[ib];
+            }
+        }
     }
+    return bloc_non_vide;
+
+}
 
 //On passe le bit R (REFER) à 1 lors d'une lecture et on incremente le compteur
 
     void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) {
 
+        int i;
         // Si le compteur atteint la periode de déréferencement, on reinitialise tous les blocs R à 0
 
-        if(pcache->pstrategy == pcache->nderef) {
-            for (int i = 0; i < pcache->nblocks; ++i) {
+        if(pcache->pstrategy >= pcache->nderef) {
+
+            for (i = 0; i < pcache->nblocks; ++i) {
                 pcache->headers[i].flags = pcache->headers[i].flags & ~REFER;
             }
             pcache->pstrategy = 0;
@@ -88,10 +104,11 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) {
 
     void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh) {
 
+        int i;
         // Si le compteur atteint la periode de déréferencement, on reinitialise tous les blocs R à 0
 
-        if(pcache->pstrategy == pcache->nderef) {
-            for (int i = 0; i < pcache->nblocks; ++i) {
+        if(pcache->pstrategy >= pcache->nderef) {
+            for (i = 0; i < pcache->nblocks; ++i) {
                 pcache->headers[i].flags = pcache->headers[i].flags & ~REFER;
             }
             pcache->pstrategy = 0;
